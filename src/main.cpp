3d3f6e29665f12614bc0c95a7fa5ef8406fe22c7
alcalino-git <alanhumber333@gmail.com>
2025-02-11 18:14:35 -0600

    Added loading screen on launch

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
+#include "glibmm/main.h"
 #include <stdio.h>
 #include <gtkmm.h>
+#include <thread>
 #include "./search.hpp"
+#include "gtkmm/box.h"
+#include "gtkmm/enums.h"
+#include "gtkmm/label.h"
+#include "gtkmm/spinner.h"
 #include "package.hpp"
 #include "database.hpp"
 
@@ -10,14 +16,33 @@ class DefaultWindow : public Gtk::Window{
 	PackageDisplay package_display;
 	SearchComponent search_component;
 	PackageDatabase* database;
+	Gtk::Spinner loading_spinner;
 
 	public:
 	DefaultWindow() {
 		set_title("Pacmanager");
 		set_size_request(1200, 800);
 
-		this->database = new PackageDatabase();
-		this->search_component.connect_database(this->database);
+		auto loading_box = Gtk::Box();
+		loading_box.set_orientation(Gtk::Orientation::VERTICAL);
+		loading_box.set_halign(Gtk::Align::CENTER);
+		loading_box.set_valign(Gtk::Align::CENTER);
+		loading_box.set_hexpand(true);
+		loading_box.set_vexpand(true);
+
+		auto loading_text = Gtk::Label("Loading package database, please wait (I promise it's not hanged!)");
+		loading_box.set_halign(Gtk::Align::CENTER);
+
+		this->loading_spinner.set_spinning(true);
+		this->loading_spinner.set_size_request(200,200);
+		this->loading_spinner.set_expand(true);
+
+		loading_box.append(loading_spinner);
+		loading_box.append(loading_text);
+
+		this->set_child(loading_box);
+
+
 		this->search_component.signal_changed().connect([this](auto p) {
 			this->package_display.set_package(p);
 		});
@@ -27,7 +52,7 @@ class DefaultWindow : public Gtk::Window{
 			this->database->update_package_state(p);
 		});
 
-		//Use a Gtk::Paned to render both the search component and PackageDisplay component 
+		//Use a Gtk::Paned to render both the search component and PackageDisplay component
 		this->screen_split.set_start_child(search_component);
 		this->screen_split.set_end_child(package_display);
 		this->screen_split.set_shrink_end_child(false);
@@ -36,7 +61,13 @@ class DefaultWindow : public Gtk::Window{
 		this->screen_split.set_resize_start_child(true);
 		this->screen_split.set_hexpand(true);
 
-		this->set_child(screen_split);
+		Glib::signal_idle().connect_once([this](){
+			this->database = new PackageDatabase();
+			this->search_component.connect_database(this->database);
+			this->set_child(screen_split);
+		});
+
+
 	}
 };
 
